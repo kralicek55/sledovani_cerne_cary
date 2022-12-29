7552176fc0d76075c4527d33593114c2d4a75bf9
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-12-29 18:10:29 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+input.onSound(DetectedSound.Loud, function () {
+    muze_jet = 1
+})
+let muze_jet = 0
+muze_jet = 0
 basic.showLeds(`
     . . . # .
     # . # . #
@@ -6,11 +11,13 @@ basic.showLeds(`
     . # . . .
     `)
 basic.forever(function () {
-    if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
-        cuteBot.motors(24, 10)
-    }
-    if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
-        cuteBot.motors(11, 24)
+    if (muze_jet == 1) {
+        if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+            cuteBot.motors(24, 10)
+        }
+        if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+            cuteBot.motors(11, 24)
+        }
         if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
             cuteBot.motors(24, 24)
         }
