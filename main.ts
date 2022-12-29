87bdd68f86b76dd074345eb75ec0a8970b578043
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2022-12-29 18:03:42 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,12 +7,12 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
-        cuteBot.motors(50, 26)
+        cuteBot.motors(24, 10)
     }
     if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
-        cuteBot.motors(24, 50)
+        cuteBot.motors(11, 24)
         if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
-            cuteBot.motors(50, 50)
+            cuteBot.motors(24, 24)
         }
     }
 })
